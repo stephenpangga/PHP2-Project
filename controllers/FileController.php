3eda2043e74b6279f9aec837336d8412a9485562
stephenpangga <42969061+stephenpangga@users.noreply.github.com>
2020-02-21 23:26:06 +0100

    fixing upload button

--- controllers/FileController.php
+++ controllers/FileController.php
@@ -4,12 +4,36 @@ class FileController extends Controller
 {
     public function index()
     {
-        return self::view("file");
+        $user= Session::get('user')->getName();
+        $userId=Session::get('user')->getId();
+        
+        if(is_dir("filemanager/.$userId.id.$user.-folder") == false)
+        {
+        //create the folder for the specific user.
+        //folder name will be userid.id.name.-folder
+            mkdir("filemanager/.$userId.id.$user.-folder");
+            return self::view("file");
+        }
+        else
+        {
+            //echo "it already exist";
+            $files = scandir("filemanager/.$userId.id.$user.-folder");
+            return self::view("file");
+        }
     }
 
     public function upload()
     {
-        echo "uploading the file";
+        //echo "uploading the file";
+        $user= Session::get('user')->getName();
+        $userId=Session::get('user')->getId();
+
+        $folder = "filemanager/.$userId.id.$user.-folder";
+        $target_file = $folder . basename($_FILES['fileToUpload']['name']);
+        $uploadOk =1;
+        $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+
+        
     }
 
     public function delete()
--- views/file.view.php
+++ views/file.view.php
@@ -10,16 +10,29 @@
             <th> Action</th>
         </tr>
     </thead>
+    <?php 
+        $user= Session::get('user')->getName();
+        $userId=Session::get('user')->getId();
+            $files = scandir("filemanager/.$userId.id.$user.-folder");
+    foreach($files as $file):
+    //i need a checker to not show directory files?>
     <tr>
-        <td> filename </td>
-        <td> file type </td>
-        <td> <button> <a href = "file/delete"> Delete </button> </td>
+        <?php if(is_dir($file) == 'directory'):?>
+            <?php else:?>
+                <td> <?= $file?> </td>
+                <td> <?= is_dir($file)? 'jpg' : 'regular file'?> </td>
+                <td> <a href = "file/delete" ><button> Delete </button> </a></td>
+            <?php endif;?>
     </tr>
+    <?php endforeach;?>
 </table>
 </body>
 
+<?php
+///onclick = "deleteFile(this.id)" id="'. $filename .'"
+?>
 
-<form method = "POST" action = "file/upload">
-<input type= "file" name = "fileToUpload" id ="fileToUpload">
-<button> Upload </button>
+<form name= "upload" method = "POST" action = "file/upload" enctype="multipart/form-data">
+<input type= "file" name="fileToUpload" id ="fileToUpload">
+<button type = "submit"> Upload </button>
 </form>
