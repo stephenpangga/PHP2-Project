60ccd93053528a69ddf25dfcb1bb2f8c6e662cd1
stephenpangga <42969061+stephenpangga@users.noreply.github.com>
2020-03-01 11:01:40 +0100

    trynna fix error with autoload

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -4,32 +4,32 @@
 spl_autoload_register(function ($class_name)
 {
     //database folder
-    if(is_file('core/database/'.$class_name.'.php'))
+    if(is_file('core/database/' .$class_name. '.php'))
     {
-    include_once 'core/database/'.$class_name.'.php';
+    include_once 'core/database/' .$class_name. '.php';
     }
     //core folder
-    else if(is_file('core/'.$class_name.'.php'))
+    else if(is_file('core/'. $class_name .'.php'))
     {
-        include_once 'core/'.$class_name.'.php';
+        include_once 'core/'. $class_name .'.php';
     }
     //router folder
-    else if(is_file('core/router/'.$class_name.'.php'))
+    else if(is_file('core/router/'. $class_name .'.php'))
     {
-        include_once 'core/router/'.$class_name.'.php';
+        include_once 'core/router/'. $class_name .'.php';
     }
     //controller folder
-    else if(is_file('controllers/'.$class_name.'.php'))
+    else if(is_file('controllers/'. $class_name .'.php'))
     {
-        include_once 'controllers/'.$class_name.'.php';
+        include_once 'controllers/'. $class_name .'.php';
     }
-    else if(is_file('models/'.$class_name.'.php'))
+    else if(is_file('models/'. $class_name .'.php'))
     {
-        include_once 'models/'.$class_name.'.php';
+        include_once 'models/'. $class_name .'.php';
     }
-    else if(is_file('service/'.$class_name.'.php'))
+    else if(is_file('service/'. $class_name .'.php'))
     {
-        include_once 'service/'.$class_name.'.php';
+        include_once 'service/'. $class_name .'.php';
     }
     else
     {
@@ -45,7 +45,7 @@ spl_autoload_register(function ($class_name)
 //require 'Request.php';
 
 
-Container::setKey('config', require 'config.php');
+Container::setKey('config', require './config/config.php');
 
 Container::setKey('db', new QueryBuilder(
     Connection::make(Container::getKey('config')['database'])
@@ -53,7 +53,7 @@ Container::setKey('db', new QueryBuilder(
 
 Session::init();
 
-
+?>
 
 
 
--- views/partials/head.php
+++ views/partials/head.php
@@ -3,7 +3,7 @@
 <html lang ="en">
 
 <head>
-    <meta charset="UTF-8">
+    <meta charset = "UTF-8">
     <title> SISA's FIle Management</title>
 </head>
 
